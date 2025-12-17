3dea2c3954fa2e4727d17cc7a54e737f192f6f08
Mwiteneza emelyne <mwitenezaemelyne184@gmail.com>
2025-12-17 15:40:31 +0200

    add of script

--- task_5.sql
+++ task_5.sql
@@ -1,1 +1,11 @@
+-- task_5.sql
 
+CREATE TABLE IF NOT EXISTS customer (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL
+);
+
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
