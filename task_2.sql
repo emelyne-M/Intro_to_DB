dde42a32602d58ca831122b1f7931f635f94d42b
Mwiteneza emelyne <mwitenezaemelyne184@gmail.com>
2025-12-17 14:36:30 +0200

    update of script

--- task_2.sql
+++ task_2.sql
@@ -27,8 +27,9 @@ CREATE TABLE IF NOT EXISTS Books (
 -- =====================================
 CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_name VARCHAR(255) NOT NULL,
-    email VARCHAR(255)
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215),
+    address TEXT
 );
 
 -- =====================================
