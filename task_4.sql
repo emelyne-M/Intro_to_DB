6794ab9932f2845fd8d03554d1f0bbe69422523d
Mwiteneza emelyne <mwitenezaemelyne184@gmail.com>
2025-12-17 15:43:35 +0200

    add of task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,4 +1,15 @@
 
+-- task_4.sql
 
-INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+SELECT 
+    COLUMN_NAME AS 'Column',
+    COLUMN_TYPE AS 'Type',
+    IS_NULLABLE AS 'Null',
+    COLUMN_KEY AS 'Key',
+    COLUMN_DEFAULT AS 'Default',
+    EXTRA AS 'Extra'
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' 
+    AND TABLE_NAME = 'Books';
